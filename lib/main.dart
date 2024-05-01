e3bf15c4ffd5f0ad61b06e59e38ae77b832b9795
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-05-01 10:51:27 +0200

    splash screen part 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sophiee/custom_material_app.dart';
 import 'package:sophiee/firebase_options.dart';
@@ -14,9 +15,11 @@ import 'package:sophiee/simple_observe_bloc.dart';
 
 void main() async {
   Bloc.observer = SimpleBlocObserver();
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await _init();
+  FlutterNativeSplash.remove();
 }
 
 _init() async {
